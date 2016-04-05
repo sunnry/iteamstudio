42265ba8d2fdb99156ed648175eacf4f2e340b8e
sunnry <sunhaihuan@gmail.com>
2016-04-05 22:13:16 +0800

    add password reset and password reset done template and views

--- profiling/urls.py
+++ profiling/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
         url(r'^$',views.profiling,name="user_profile"),
         url(r'^changepassword/$',views.changepassword,name="user_changepassword"),
+        url(r'^resetpassword/$',views.resetpassword,name="user_resetpassword"),
+        url(r'^resetpassword_done/$',views.resetpassword_done,name="user_resetpassword_done"),
 ]
--- profiling/views.py
+++ profiling/views.py
@@ -5,7 +5,7 @@ from django.views.generic.base import TemplateView
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse,reverse_lazy
-from allauth.account.views import PasswordChangeView
+from allauth.account.views import PasswordChangeView,PasswordResetView,PasswordResetDoneView
 
 class ProfileView(TemplateView):
     template_name = 'accounts/profile.html'
@@ -49,3 +49,27 @@ class CustomerPasswordChangeView(PasswordChangeView):
         return ret
 
 changepassword = CustomerPasswordChangeView.as_view()
+
+
+class CustomerPasswordResetView(PasswordResetView):
+    template_name = 'accounts/profile.html'
+    success_url = reverse_lazy('profiling:user_resetpassword_done')
+
+    def get_context_data(self,**kwargs):
+        ret = super(CustomerPasswordResetView,self).get_context_data(**kwargs)
+        ret.update({'tab':'resetpassword'})
+        return ret
+
+
+resetpassword = CustomerPasswordResetView.as_view()
+
+
+class CustomerPasswordResetDoneView(PasswordResetDoneView):
+    template_name = 'accounts/profile.html'
+
+    def get_context_data(self,**kwargs):
+        ret = super(CustomerPasswordResetDoneView,self).get_context_data(**kwargs)
+        ret.update({'tab':'resetpassword_done'})
+        return ret
+
+resetpassword_done = CustomerPasswordResetDoneView.as_view()

a3dc4700f6f427792d7cbc43daf6d5efa693fdfc
sunnry <sunhaihuan@gmail.com>
2016-04-11 22:15:15 +0800

    can update user profiles now,include profile image file

--- profiling/forms.py
+++ profiling/forms.py
@@ -31,22 +31,44 @@ class ProfileForm(forms.Form):
     #can add your additional clean code
     def clean(self):
         super(ProfileForm,self).clean()
-        if self._errors:
-            #pdb.set_trace()
-            return
+
+        try:
+            userProfileImgFile = self.cleaned_data['avatar']
+            imageType = userProfileImgFile.content_type
+            if isinstance(imageType,unicode):
+                imageType = imageType.encode('ascii','ignore')
+
+            if imageType!='image/jpeg':
+                raise forms.ValidationError('you have select a wrong image file format, pls select .img format')
+        except KeyError:
+            pass
+
         return self.cleaned_data
 
     def save(self,request):
         if request.user.is_authenticated():
             self.user = request.user
-            f = request.FILES['avatar']
-            p = self.cleaned_data['phone']
-            d = self.cleaned_data['display_username']
-            l = self.cleaned_data['location']
-            i = self.cleaned_data['interest']
-            g = self.cleaned_data['gender']
-            account = UserAccount(user=self.user,user_displayname=d,user_phone=p,user_location=l,user_interest=i,user_gender=g,user_avatar=f)
-            account.save()
-            return True
+            try:
+                account = UserAccount.objects.get(user=self.user)
+                if account.user_avatar:
+                    account.user_avatar.delete()
+                account.user_avatar = self.cleaned_data['avatar']
+                account.user_displayname = self.cleaned_data['display_username']
+                account.user_phone = self.cleaned_data['phone']
+                account.user_location = self.cleaned_data['location']
+                account.user_interest = self.cleaned_data['interest']
+                account.user_gender = self.cleaned_data['gender']
+                account.save()
+                return True
+            except UserAccount.DoesNotExist:
+                f = request.FILES['avatar']
+                p = self.cleaned_data['phone']
+                d = self.cleaned_data['display_username']
+                l = self.cleaned_data['location']
+                i = self.cleaned_data['interest']
+                g = self.cleaned_data['gender']
+                account = UserAccount(user=self.user,user_displayname=d,user_phone=p,user_location=l,user_interest=i,user_gender=g,user_avatar=f)
+                account.save()
+                return True
 
         return False
